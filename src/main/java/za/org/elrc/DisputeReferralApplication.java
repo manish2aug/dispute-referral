c6922d4ca292d1c6eb44c9abdf0ab5ca64852749
manish2aug <manish2aug@gmail.com>
2018-09-22 22:02:18 +0200

    Initial commit

---
+++ src/main/java/za/org/elrc/DisputeReferralApplication.java
@@ -0,0 +1,12 @@
+package za.org.elrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisputeReferralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisputeReferralApplication.class, args);
+	}
+}
---
+++ src/test/java/za/org/elrc/DisputeReferralApplicationTests.java
@@ -0,0 +1,16 @@
+package za.org.elrc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DisputeReferralApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
